78071dd06844785e5cc0f5acd75bdd6dab21a6a3
David Eisinger <david.eisinger@gmail.com>
2014-12-10 23:52:42 -0500

    Add some comments

--- decrypt.ml
+++ decrypt.ml
@@ -1,23 +1,28 @@
+(* convert string to list of chars *)
 let rec char_list str = match str with
   | "" -> []
   | s  -> String.get s 0 :: char_list (String.sub s 1 ((String.length s) - 1))
 
+(* convert two hex chars into an int *)
 let int_from_hex_chars c1 c2 =
   int_of_string ("0x" ^ (String.make 1 c1) ^ (String.make 1 c2))
 
+(* get the mask value at the given index *)
 let get_mask key index =
   let c1 = List.nth key (index mod (List.length key)) in
   let c2 = List.nth key ((index + 1) mod (List.length key)) in
   int_from_hex_chars c1 c2
 
+(* given a list of ints, convert to ASCII char and then concatenate *)
 let rec string_from_ints nums = match nums with
   | [] -> ""
   | n :: ns -> (String.make 1 (Char.chr n)) ^ (string_from_ints ns)
 
+(* convert the ciphertext to int values representing the plaintext *)
 let rec decrypt ciphertext key index = match ciphertext with
   | [] -> []
-  | c1 :: c2 :: cs -> let mask = get_mask key index in
-    ((int_from_hex_chars c1 c2) lxor mask) :: decrypt cs key (index + 2)
+  | c1 :: c2 :: cs ->
+    ((int_from_hex_chars c1 c2) lxor (get_mask key index)) :: decrypt cs key (index + 2)
   | _ :: [] -> failwith "Invalid ciphertext";;
 
 let key = char_list (Array.get Sys.argv 1) in
--- encrypt.ml
+++ encrypt.ml
@@ -1,23 +1,27 @@
+(* convert string to list of chars *)
 let rec char_list str = match str with
   | "" -> []
   | s  -> String.get s 0 :: char_list (String.sub s 1 ((String.length s) - 1))
 
+(* convert two hex chars into an int *)
 let int_from_hex_chars c1 c2 =
   int_of_string ("0x" ^ (String.make 1 c1) ^ (String.make 1 c2))
 
+(* get the mask value at the given index *)
 let get_mask key index =
   let c1 = List.nth key (index mod (List.length key)) in
   let c2 = List.nth key ((index + 1) mod (List.length key)) in
   int_from_hex_chars c1 c2
 
+(* given a list of numbers, create strings of hex values and concatenate *)
 let rec hex_string nums = match nums with
   | [] -> ""
   | n :: ns -> Printf.sprintf "%x" n ^ hex_string ns
 
+(* convert the plaintext to int values representing the ciphertext *)
 let rec encrypt message key index = match message with
   | [] -> []
-  | c :: cs -> let mask = get_mask key index in
-    ((Char.code c) lxor mask) :: encrypt cs key (index + 2);;
+  | c :: cs -> ((Char.code c) lxor (get_mask key index)) :: encrypt cs key (index + 2);;
 
 let key = char_list (Array.get Sys.argv 1) in
   let buffer = String.make 1000 '.' in
