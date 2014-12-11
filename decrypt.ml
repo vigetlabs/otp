f8ba774351286dade01e5f0009090c10a814c475
David Eisinger <david.eisinger@gmail.com>
2014-12-11 15:45:59 -0500

    Use let/and syntax over nested lets

--- decrypt.ml
+++ decrypt.ml
@@ -8,8 +8,8 @@ let string_of_ints nums = String.of_list (List.map Char.chr nums)
 
 (* get the mask value for the next two characters of the key *)
 let get_mask key =
-  let c1 = BatDllist.get key in
-  let c2 = BatDllist.get (BatDllist.next key) in
+  let c1 = BatDllist.get key
+  and c2 = BatDllist.get (BatDllist.next key) in
   int_of_hex_chars c1 c2
 
 (* convert the ciphertext to int values representing the plaintext *)
@@ -19,6 +19,6 @@ let rec decrypt ciphertext key = match ciphertext with
     (int_of_hex_chars c1 c2) lxor (get_mask key) :: decrypt cs (BatDllist.skip key 2)
   | _ :: [] -> failwith "Invalid ciphertext";;
 
-let key = BatDllist.of_list (String.to_list (Array.get Sys.argv 1)) in
-  let ciphertext = String.to_list (BatIO.read_line BatIO.stdin) in
+let key = BatDllist.of_list (String.to_list (Array.get Sys.argv 1))
+  and ciphertext = String.to_list (BatIO.read_line BatIO.stdin) in
   print_string (string_of_ints (decrypt ciphertext key))
--- encrypt.ml
+++ encrypt.ml
@@ -8,8 +8,8 @@ let hex_string_of_ints nums = String.concat "" (List.map (Printf.sprintf "%x") n
 
 (* get the mask value for the next two characters of the key *)
 let get_mask key =
-  let c1 = BatDllist.get key in
-  let c2 = BatDllist.get (BatDllist.next key) in
+  let c1 = BatDllist.get key
+  and c2 = BatDllist.get (BatDllist.next key) in
   int_of_hex_chars c1 c2
 
 (* convert the plaintext to int values representing the ciphertext *)
@@ -17,6 +17,6 @@ let rec encrypt message key = match message with
   | [] -> []
   | c :: cs -> (Char.code c) lxor (get_mask key) :: encrypt cs (BatDllist.skip key 2);;
 
-let key = BatDllist.of_list (String.to_list (Array.get Sys.argv 1)) in
-  let message = String.to_list (BatIO.read_line BatIO.stdin) in
+let key = BatDllist.of_list (String.to_list (Array.get Sys.argv 1))
+  and message = String.to_list (BatIO.read_line BatIO.stdin) in
   print_string (hex_string_of_ints (encrypt message key))
