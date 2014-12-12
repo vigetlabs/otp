328d362cd13d1883aba9e28a623eca674c712a9a
David Eisinger <david.eisinger@gmail.com>
2014-12-12 15:01:21 -0500

    Guess 'Bat' isn't required

--- decrypt.ml
+++ decrypt.ml
@@ -8,17 +8,17 @@ let string_of_ints nums = String.of_list (List.map Char.chr nums)
 
 (* get the mask value for the next two characters of the key *)
 let get_mask key =
-  let c1 = BatDllist.get key
-  and c2 = BatDllist.get (BatDllist.next key) in
+  let c1 = Dllist.get key
+  and c2 = Dllist.get (Dllist.next key) in
   int_of_hex_chars c1 c2
 
 (* convert the ciphertext to int values representing the plaintext *)
 let rec decrypt ciphertext key = match ciphertext with
   | [] -> []
   | c1 :: c2 :: cs ->
-    (int_of_hex_chars c1 c2) lxor (get_mask key) :: decrypt cs (BatDllist.skip key 2)
+    (int_of_hex_chars c1 c2) lxor (get_mask key) :: decrypt cs (Dllist.skip key 2)
   | _ :: [] -> failwith "Invalid ciphertext";;
 
-let key = BatDllist.of_list (String.to_list (Array.get Sys.argv 1))
-  and ciphertext = String.to_list (BatIO.read_line BatIO.stdin) in
+let key = Dllist.of_list (String.to_list (Array.get Sys.argv 1))
+  and ciphertext = String.to_list (IO.read_line IO.stdin) in
   print_string (string_of_ints (decrypt ciphertext key))
--- encrypt.ml
+++ encrypt.ml
@@ -8,15 +8,15 @@ let hex_string_of_ints nums = String.concat "" (List.map (Printf.sprintf "%x") n
 
 (* get the mask value for the next two characters of the key *)
 let get_mask key =
-  let c1 = BatDllist.get key
-  and c2 = BatDllist.get (BatDllist.next key) in
+  let c1 = Dllist.get key
+  and c2 = Dllist.get (Dllist.next key) in
   int_of_hex_chars c1 c2
 
 (* convert the plaintext to int values representing the ciphertext *)
 let rec encrypt message key = match message with
   | [] -> []
-  | c :: cs -> (Char.code c) lxor (get_mask key) :: encrypt cs (BatDllist.skip key 2);;
+  | c :: cs -> (Char.code c) lxor (get_mask key) :: encrypt cs (Dllist.skip key 2);;
 
-let key = BatDllist.of_list (String.to_list (Array.get Sys.argv 1))
-  and message = String.to_list (BatIO.read_line BatIO.stdin) in
+let key = Dllist.of_list (String.to_list (Array.get Sys.argv 1))
+  and message = String.to_list (IO.read_line IO.stdin) in
   print_string (hex_string_of_ints (encrypt message key))
