0400cc2a6960798e329c9841fd6d910f47b4f6aa
David Eisinger <david.eisinger@gmail.com>
2014-12-23 11:43:00 -0500

    Add comments; refactor

--- languages/Haskell/decrypt.hs
+++ languages/Haskell/decrypt.hs
@@ -3,15 +3,16 @@ import Data.Char(chr)
 import System.Environment(getArgs)
 import Text.Printf(printf)
 
+-- convert two hex chars into an int
 intOfHexChars c1 c2 = read (printf "0x%c%c" c1 c2)
 
-stringOfInts nums = map chr nums 
-
+-- get the mask value for the next two characters of the key
 getMask key idx = intOfHexChars c1 c2
   where
     c1 = key !! mod idx (length key)
     c2 = key !! mod (idx + 1) (length key)
 
+-- convert the ciphertext to int values representing the plaintext
 decrypt ciphertext key idx = case ciphertext of
   [] -> []
   c1:c2:cs -> xor (intOfHexChars c1 c2) (getMask key idx) : decrypt cs key (idx + 2)
@@ -19,4 +20,4 @@ decrypt ciphertext key idx = case ciphertext of
 main = do
   plaintext <- getLine
   args <- getArgs
-  putStr (stringOfInts (decrypt plaintext (head args) 0))
+  putStr (map chr (decrypt plaintext (head args) 0))
--- languages/Haskell/encrypt.hs
+++ languages/Haskell/encrypt.hs
@@ -4,15 +4,19 @@ import Data.List(concat)
 import System.Environment(getArgs)
 import Text.Printf(printf)
 
+-- convert two hex chars into an int
 intOfHexChars c1 c2 = read (printf "0x%c%c" c1 c2)
 
+-- given a list of numbers, create strings of hex values and concatenate
 hexStringOfInts nums = concat (map (printf "%x") nums)
 
+-- get the mask value for the next two characters of the key
 getMask key idx = intOfHexChars c1 c2
   where
     c1 = key !! mod idx (length key)
     c2 = key !! mod (idx + 1) (length key)
 
+-- convert the plaintext to int values representing the ciphertext
 encrypt plaintext key idx = case plaintext of
   [] -> []
   c:cs -> xor (ord c) (getMask key idx) : encrypt cs key (idx + 2)
