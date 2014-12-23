2336b3a04f48526c9efb754aa83c8875f11b264d
David Eisinger <david.eisinger@gmail.com>
2014-12-23 11:33:05 -0500

    Working Haskell implementation

--- languages/Haskell/decrypt.hs
+++ languages/Haskell/decrypt.hs
@@ -1,23 +1,22 @@
 import Data.Bits(xor)
-import Data.Char(ord)
-import Data.List(concat)
+import Data.Char(chr)
 import System.Environment(getArgs)
 import Text.Printf(printf)
 
 int_of_hex_chars c1 c2 = read (printf "0x%c%c" c1 c2)
 
-hex_string_of_ints nums = concat (map (printf "%x") nums)
+string_of_ints nums = map chr nums 
 
 get_mask key idx = int_of_hex_chars c1 c2
   where
     c1 = key !! mod idx (length key)
     c2 = key !! mod (idx + 1) (length key)
 
-encrypt plaintext key idx = case plaintext of
+decrypt ciphertext key idx = case ciphertext of
   [] -> []
-  c:cs -> xor (ord c) (get_mask key idx) : encrypt cs key (idx + 2)
+  c1:c2:cs -> xor (int_of_hex_chars c1 c2) (get_mask key idx) : decrypt cs key (idx + 2)
 
 main = do
   plaintext <- getLine
   args <- getArgs
-  putStr (hex_string_of_ints (encrypt plaintext (head args) 0))
+  putStr (string_of_ints (decrypt plaintext (head args) 0))
