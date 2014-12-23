b14f5332f91d6b66e95a52f6837170148b969ad3
David Eisinger <david.eisinger@gmail.com>
2014-12-23 13:41:10 -0500

    Add type signatures to top-level methods
    And use Haddock-style comments.

--- languages/Haskell/decrypt.hs
+++ languages/Haskell/decrypt.hs
@@ -3,21 +3,25 @@ import Data.Char(chr)
 import System.Environment(getArgs)
 import Text.Printf(printf)
 
--- convert two hex chars into an int
+-- | Convert two hex chars into an int
+intOfHexChars :: Char -> Char -> Int
 intOfHexChars c1 c2 = read (printf "0x%c%c" c1 c2)
 
--- get the mask value for the next two characters of the key
+-- | Get the mask value for the next two characters of the key
+getMask :: [Char] -> Int -> Int
 getMask key idx = intOfHexChars c1 c2
   where
     c1 = key !! mod idx (length key)
     c2 = key !! mod (idx + 1) (length key)
 
--- convert the ciphertext to int values representing the plaintext
+-- | Convert the ciphertext to int values representing the plaintext
+decrypt :: [Char] -> [Char] -> Int -> [Int]
 decrypt ciphertext key idx = case ciphertext of
   [] -> []
   c1:c2:cs -> xor (intOfHexChars c1 c2) (getMask key idx) : decrypt cs key (idx + 2)
   _:[] -> error "Invalid ciphertext"
 
+main :: IO ()
 main = do
   plaintext <- getLine
   args <- getArgs
