b0d867394120c05a36f980c823742d5cd4012706
David Eisinger <david.eisinger@gmail.com>
2014-12-23 11:39:23 -0500

    Camelcase instead of snake

--- languages/Haskell/decrypt.hs
+++ languages/Haskell/decrypt.hs
@@ -3,20 +3,20 @@ import Data.Char(chr)
 import System.Environment(getArgs)
 import Text.Printf(printf)
 
-int_of_hex_chars c1 c2 = read (printf "0x%c%c" c1 c2)
+intOfHexChars c1 c2 = read (printf "0x%c%c" c1 c2)
 
-string_of_ints nums = map chr nums 
+stringOfInts nums = map chr nums 
 
-get_mask key idx = int_of_hex_chars c1 c2
+getMask key idx = intOfHexChars c1 c2
   where
     c1 = key !! mod idx (length key)
     c2 = key !! mod (idx + 1) (length key)
 
 decrypt ciphertext key idx = case ciphertext of
   [] -> []
-  c1:c2:cs -> xor (int_of_hex_chars c1 c2) (get_mask key idx) : decrypt cs key (idx + 2)
+  c1:c2:cs -> xor (intOfHexChars c1 c2) (getMask key idx) : decrypt cs key (idx + 2)
 
 main = do
   plaintext <- getLine
   args <- getArgs
-  putStr (string_of_ints (decrypt plaintext (head args) 0))
+  putStr (stringOfInts (decrypt plaintext (head args) 0))
--- languages/Haskell/encrypt.hs
+++ languages/Haskell/encrypt.hs
@@ -4,20 +4,20 @@ import Data.List(concat)
 import System.Environment(getArgs)
 import Text.Printf(printf)
 
-int_of_hex_chars c1 c2 = read (printf "0x%c%c" c1 c2)
+intOfHexChars c1 c2 = read (printf "0x%c%c" c1 c2)
 
-hex_string_of_ints nums = concat (map (printf "%x") nums)
+hexStringOfInts nums = concat (map (printf "%x") nums)
 
-get_mask key idx = int_of_hex_chars c1 c2
+getMask key idx = intOfHexChars c1 c2
   where
     c1 = key !! mod idx (length key)
     c2 = key !! mod (idx + 1) (length key)
 
 encrypt plaintext key idx = case plaintext of
   [] -> []
-  c:cs -> xor (ord c) (get_mask key idx) : encrypt cs key (idx + 2)
+  c:cs -> xor (ord c) (getMask key idx) : encrypt cs key (idx + 2)
 
 main = do
   plaintext <- getLine
   args <- getArgs
-  putStr (hex_string_of_ints (encrypt plaintext (head args) 0))
+  putStr (hexStringOfInts (encrypt plaintext (head args) 0))
