5c4c43e7f0ae6c552077a7ec745be32d8a57a490
David Eisinger <david.eisinger@gmail.com>
2015-01-14 09:59:27 -0500

    Haskell: use 'cycle' for circular key

--- languages/Haskell/decrypt.hs
+++ languages/Haskell/decrypt.hs
@@ -4,25 +4,22 @@ import System.Environment(getArgs)
 import Text.Printf(printf)
 
 -- | Convert two hex chars into an int
-intOfHexChars :: Char -> Char -> Int
-intOfHexChars c1 c2 = read $ printf "0x%c%c" c1 c2
+intOfHexChars :: [Char] -> Int
+intOfHexChars cs = read $ printf "0x%c%c" c1 c2 where [c1, c2] = cs
 
 -- | Get the mask value for the next two characters of the key
-getMask :: [Char] -> Int -> Int
-getMask key idx = intOfHexChars c1 c2
-  where
-    c1 = key !! mod idx (length key)
-    c2 = key !! mod (idx + 1) (length key)
+getMask :: [Char] -> Int
+getMask key = intOfHexChars $ take 2 key
 
 -- | Convert the ciphertext to int values representing the plaintext
-decrypt :: [Char] -> [Char] -> Int -> [Int]
-decrypt ciphertext key idx = case ciphertext of
+decrypt :: [Char] -> [Char] -> [Int]
+decrypt ciphertext key = case ciphertext of
   []       -> []
   _:[]     -> error "Invalid ciphertext"
-  c1:c2:cs -> xor (intOfHexChars c1 c2) (getMask key idx) : decrypt cs key (idx + 2)
+  c1:c2:cs -> xor (intOfHexChars [c1, c2]) (getMask key) : decrypt cs (drop 2 key)
 
 main :: IO ()
 main = do
   plaintext <- getLine
   args <- getArgs
-  putStr $ map chr $ decrypt plaintext (head args) 0
+  putStr $ map chr $ decrypt plaintext $ cycle $ head args
--- languages/Haskell/encrypt.hs
+++ languages/Haskell/encrypt.hs
@@ -4,28 +4,25 @@ import System.Environment(getArgs)
 import Text.Printf(printf)
 
 -- | Convert two hex chars into an int
-intOfHexChars :: Char -> Char -> Int
-intOfHexChars c1 c2 = read $ printf "0x%c%c" c1 c2
+intOfHexChars :: [Char] -> Int
+intOfHexChars cs = read $ printf "0x%c%c" c1 c2 where [c1, c2] = cs
 
 -- | Given a list of numbers, create strings of hex values and concatenate
 hexStringOfInts :: [Int] -> [Char]
 hexStringOfInts nums = concat $ map (printf "%x") nums
 
 -- | Get the mask value for the next two characters of the key
-getMask :: [Char] -> Int -> Int
-getMask key idx = intOfHexChars c1 c2
-  where
-    c1 = key !! mod idx (length key)
-    c2 = key !! mod (idx + 1) (length key)
+getMask :: [Char] -> Int
+getMask key = intOfHexChars $ take 2 key
 
 -- | Convert the plaintext to int values representing the ciphertext
-encrypt :: [Char] -> [Char] -> Int -> [Int]
-encrypt plaintext key idx = case plaintext of
+encrypt :: [Char] -> [Char] -> [Int]
+encrypt plaintext key = case plaintext of
   []   -> []
-  c:cs -> xor (ord c) (getMask key idx) : encrypt cs key (idx + 2)
+  c:cs -> xor (ord c) (getMask key) : encrypt cs (drop 2 key)
 
 main :: IO ()
 main = do
   plaintext <- getLine
   args <- getArgs
-  putStr $ hexStringOfInts $ encrypt plaintext (head args) 0
+  putStr $ hexStringOfInts $ encrypt plaintext $ cycle $ head args
