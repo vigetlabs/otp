bcbea6f0497c8ad31270dc7b8acda2d32cce7bc3
Patrick Reagan <reaganpr@gmail.com>
2015-02-19 13:12:44 -0700

    Prefer `isxdigit` over custom implementation

--- languages/C/convert.c
+++ languages/C/convert.c
@@ -1,21 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+
 #include "convert.h"
 
 #define HEX_LOWER_OFFSET ('a' - 10)
 #define HEX_UPPER_OFFSET ('A' - 10)
-
-// Check if a single character is a valid hexadecimal digit
-short is_hex_digit(char digit)
-{
-    short is_hex_digit = 0;
-
-    is_hex_digit = is_hex_digit || (digit >= '0' && digit <= '9');
-    is_hex_digit = is_hex_digit || (digit >= 'A' && digit <= 'F');
-    is_hex_digit = is_hex_digit || (digit >= 'a' && digit <= 'f');
-
-    return is_hex_digit;
-}
 
 // Check all characters in a string to ensure they are hexadecimal digits
 short is_hex_key(char *key, int length)
@@ -24,7 +14,7 @@ short is_hex_key(char *key, int length)
     int   i      = 0;
 
     for (i = 0; i < length; i++) {
-        is_hex = is_hex && is_hex_digit(key[i]);
+        is_hex = is_hex && isxdigit(key[i]);
     }
 
     return is_hex;
